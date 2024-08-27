e62b671fe7f54b6970cb69724bd4138ac9b919e0
xoaeb <shoaib.salim@npstx.com>
2024-08-27 22:37:52 +0530

    "Updated HTML templates for amount-enter, landing, link-UPI, and upi components, including changes to logos, images, and layout."

--- src/app/link-UPI/link-UPI.component.ts
+++ src/app/link-UPI/link-UPI.component.ts
@@ -4,26 +4,32 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-link-UPI',
   templateUrl: './link-UPI.component.html',
-  styleUrls: ['./link-UPI.component.css']
+  styleUrls: ['./link-UPI.component.css'],
 })
 export class LinkUPIComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
   mobileNumber: string = '';
-  showQR:boolean=false
-  ngOnInit() {
-  }
+  showQR: boolean = false;
+  isLoading = true;
+  progress = 0;
 
-  showQRCode(){
-    this.showQR=!this.showQR
+  ngOnInit() {}
 
-    setTimeout(() => {
-      this.nextPage()
-    }, 5000);
+  showQRCode() {
+    this.showQR = !this.showQR;
+    this.simulateLoading()
+
+    // setTimeout(() => {
+    //   this.isLoading = false;
+    //   this.nextPage();
+
+    // }, 5000);
+
+    // setTimeout(() => {
+    // }, 5000);
   }
-  nextPage(){
+  nextPage() {
     this.router.navigate(['/registration-success']);
-
   }
   isButtonEnabled: boolean = false;
 
@@ -32,4 +38,16 @@ export class LinkUPIComponent implements OnInit {
     this.isButtonEnabled = this.mobileNumber.length === 10;
   }
 
+  simulateLoading(): void {
+    const interval = setInterval(() => {
+      this.progress += 10;
+      if (this.progress >= 105) {
+        this.progress = 100;
+        this.isLoading = false;
+        clearInterval(interval);
+        this.nextPage(); 
+
+      }
+    }, 500); // Adjust this interval for your desired loading time
+  }
 }
